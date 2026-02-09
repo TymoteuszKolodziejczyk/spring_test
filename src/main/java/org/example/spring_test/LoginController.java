a3adf23eef9731f16f2591a0eb097b0329fd1711
tymek <Tymoteusz.Kolodziejczyk@tm1.edu.pl>
2026-02-09 13:17:03 +0100

    init2

--- src/main/java/org/example/spring_test/LoginController.java
+++ src/main/java/org/example/spring_test/LoginController.java
@@ -1,6 +1,7 @@
 package org.example.spring_test;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,11 @@ public class LoginController {
     @PostMapping("/postem")
     public String postem(@RequestParam String login, @RequestParam String pass) {
         return String.format("<h1>Login %s %s</h1>", login, pass);
+    }
+
+    @PostMapping("/postem2")
+    public String postem2(@RequestBody MyBody body) {
+        System.out.println(body.getLogin() + " " + body.getPass());
+        return String.format("<h1>Login %s %s</h1>", body.getLogin(), body.getPass());
     }
 }
---
+++ src/main/java/org/example/spring_test/MyBody.java
@@ -0,0 +1,26 @@
+package org.example.spring_test;
+
+public class MyBody {
+    String login;
+    String pass;
+
+    public MyBody() {
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
